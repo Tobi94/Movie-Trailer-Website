94e4ab242ecd7191e2e7522ebfd42385b8dfe19b
Tobias Reins <tobias.reins@student.uni-luebeck.de>
2017-12-03 18:27:41 +0100

    pep8

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,12 +3,12 @@
 import media
 import fresh_tomatoes
 
+
 def main():
     """Creates Tree Movie objects and initializes them"""
     toy_story = media.Movie("Toy Story", "A Story of a boy and his toys that come to life", "https://upload.wikimedia.org/wikipedia/commons/0/0a/Toy_Story_logo.svg", "https://www.youtube.com/watch?v=KYz2wyBy3kc", "1996")
     star_wars = media.Movie("Star Wars: The Force Awakens", "A Story of a boy and his toys that come to life", "https://upload.wikimedia.org/wikipedia/commons/2/20/Das_erwachen_der_macht_logo.svg", "https://www.youtube.com/watch?v=0IjOPqiyVik", "2015")
     transformers = media.Movie("Transformers: The Last Knight", "A Story of a boy and his toys that come to life", "https://upload.wikimedia.org/wikipedia/commons/4/49/Transformers_5_El_%C3%9Altimo_caballero.png", "https://www.youtube.com/watch?v=R4nrFuJedyk", "2016")
-
 
     # Store movies in array
     movies = [toy_story, star_wars, transformers]
