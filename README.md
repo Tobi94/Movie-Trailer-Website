ebc117a8b8d9803b6e91fdeb4f5c02b7ca30cd04
Tobias Reins <tobias.reins@student.uni-luebeck.de>
2017-12-03 19:44:48 +0100

    pep8

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ A Pyhto program that displays a number of movies in the users browser. By clicki
 This program was written with Python 2.7.14
 
 ## Quick Start
-After downloading the files, you need to run the `entertainment_center.py` file. This creates a HTML file and opens it in the webbrowser.
+1. Download the code or clone the repository
+2. Run the `entertainment_center.py` file in the terminal or use `IDLE`. To do so, open the terminal in the directory that contains the downloaded files and type `python entertainment_center.py`.
--- media.py
+++ media.py
@@ -9,10 +9,12 @@ class Movie():
         title: A string to store the title
         storyline: A string to store the storyline
         poster_image_url: A string to store the URL of the poster image
-        trailer_youtube_url: A string to store the URL of the trailer on youtube
+        trailer_youtube_url: A string to store the URL of
+            the trailer on youtube
         release_date: A string to store the release year of the movie
     """
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, movie_release):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube, movie_release):
         """Initialises Movie class instance variables."""
         self.title = movie_title
         self.storyline = movie_storyline
